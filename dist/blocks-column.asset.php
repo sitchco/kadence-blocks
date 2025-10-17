6b658b8c63d396ea2838ad5632be3b5633253aa2
Cyrus Collier <ccollier@situationinteractive.com>
2025-10-17 16:39:17 -0500

    Update dist

--- dist/blocks-column.asset.php
+++ dist/blocks-column.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('kadence-components', 'kadence-helpers', 'kadence-icons', 'lodash', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '3e1fe1e8d5a011f25120');
+<?php return array('dependencies' => array('kadence-components', 'kadence-helpers', 'kadence-icons', 'lodash', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'c8160fbdba0129ceea07');
