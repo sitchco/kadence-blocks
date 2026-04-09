c74f8d59e1293e04647af98d36cdd10a7769a2e4
Cyrus Collier <ccollier@situationinteractive.com>
2026-04-09 14:16:22 -0400

    Remove dist from tracked files

--- dist/blocks-accordion.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('kadence-components', 'kadence-helpers', 'kadence-icons', 'lodash', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'de2a6bc6f84b43cd6725');
